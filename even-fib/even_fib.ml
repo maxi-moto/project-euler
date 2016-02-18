a465118c6b9269c3c06c9fda166bd80e6e0c6b7e
Max Matsumoto <maxwell.m.matsumoto@gmail.com>
2016-02-18 07:26:16 -0800

    wip - faster fib function

--- even-fib/even_fib.ml
+++ even-fib/even_fib.ml
@@ -2,4 +2,10 @@ let rec fib number =
     match number with
     | 0 -> 1
     | 1 -> 1
-    | number -> fib (number - 1) + fib (number - 2)
+    | number -> fib (number - 1) + fib (number - 2);;
+
+let rec fib_tail index current prev =
+    match index with
+    | 0 -> prev
+    | 1 -> current
+    | index -> fib_tail (index - 5) (current + prev) (current);;
