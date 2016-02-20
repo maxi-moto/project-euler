e1fc74c44ca9586857bbcdb4789f277dc6daa7de
Max Matsumoto <maxwell.m.matsumoto@gmail.com>
2016-02-20 08:30:31 -0800

    wip - logic to add even numbers [FAILS]

--- even-fib/even_fib.ml
+++ even-fib/even_fib.ml
@@ -10,6 +10,8 @@ let rec fib_tail index current prev =
     | 1 -> current
     | index -> fib_tail (index - 1) (current + prev) (current);;
 
-let rec fib_limit current prev =
-    if current <= 4000000 then fib_limit (current + prev) (current)
+let rec fib_limit current prev sum =
+    if current <= 10 then 
+        if (current mod 2 == 0) then fib_limit (current + prev) (current) (sum + current)
+        else fib_limit (current + prev) (current) (sum)
     else prev;;
